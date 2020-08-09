fb6298e9d30ecfaeb4f35f22f0a5801909ffe4af
aawezk456 <aawezk456@gmail.com>
2020-08-09 14:02:54 +0530

    first commit

---
+++ redditapi.js
@@ -0,0 +1,10 @@
+export default {
+  search: function(searchTerm, searchLimit, sortBy) {
+    return fetch(`http://www.reddit.com/search.json?q=$
+      {searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+    )
+      .then(res => res.json())
+      .then(data => data.data.children.map(data => data.data))
+      .catch(err => console.log(err));
+  }
+};
